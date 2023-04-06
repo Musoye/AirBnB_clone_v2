00a4927b8da15db3fd2a141c18c2b2768d55165e
Musoye <oyebamijimustapha44@gmail.com>
2023-04-06 15:51:42 -0700

    Full deployment

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -3,11 +3,13 @@
 
 from fabric.api import local
 from datetime import datetime
+import os
 
 
 def do_pack():
     """The function to configure the archive"""
-    local("mkdir versions")
+    if not (os.path.isdir("versions")):
+        local("mkdir versions")
     pat = "versions/web_static_{}.tgz".format(
            datetime.strftime(datetime.now(), "%Y%m%d%H%M%S"))
     result = local("tar -czvf {} web_static".format(pat))
