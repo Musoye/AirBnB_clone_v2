944ae695d7a1d3c429d8cc5772a646a5062b8b60
oyebamiji mustapha <oyebamijimustapha44@gmail.com>
2023-04-20 22:03:22 +0100

    n is anumber'

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+"""Run a flask app that returns Hello HBNB"""
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello():
+    """return hello NMBB"""
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """return hbnb"""
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def c_is_fun(text):
+    """Configure C is Fun"""
+    text = text.replace('_', ' ')
+    return f'C {text}'
+
+
+@app.route("/python/", strict_slashes=False)
+@app.route("/python/<text>", strict_slashes=False)
+def python_is_fun(text='is cool'):
+    """Configure Python is Fun"""
+    text = text.replace('_', ' ')
+    return f'C {text}'
+
+
+@app.route("/number/<int:n>", strict_slashes=False)
+def is_number(n):
+    """Check and return if it is an integer"""
+    return f'{n} is a number'
+
+
+if __name__ == "__name__":
+    app.run(host="0.0.0.0")
